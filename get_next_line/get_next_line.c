33129fb5a64f99feb09a8ff0eddee634bdc1885a
hinve <140248223+hinve@users.noreply.github.com>
2023-11-14 16:21:40 +0100

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -106,18 +106,3 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-/* int	main(void)
-{
-	int fd;
-	char *next_line;
-
-	fd = open("test.txt", O_RDONLY);
-	next_line = get_next_line(fd);
-	next_line = get_next_line(fd);
-	next_line = get_next_line(fd);
-	next_line = get_next_line(fd);
-	next_line = get_next_line(fd);
-	next_line = get_next_line(fd);
-	printf("%s", next_line);
-	close(fd);
-} */
